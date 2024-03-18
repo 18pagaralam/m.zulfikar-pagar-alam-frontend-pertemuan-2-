3222499ccbadf1e9a4edfa42351e867d7afc322a
18pagaralam <162072594+18pagaralam@users.noreply.github.com>
2024-03-18 12:22:20 +0700

    FRONTEND PERTEMUAN 2

---
+++ src/Pages/home.jsx
@@ -0,0 +1,12 @@
+import Header from "../Components/Header";
+import Utama from "../Components/Utama";
+function Home() {
+    return ( 
+        <>
+            <Header/>
+            <Utama/>
+        </>
+     );
+}
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Contoh from './Contoh.jsx'
+import Home from './Pages/home.jsx'
+import IndukProps from './Pages/induk-props.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {/*<Contoh />*/}
+    {/*<Home />*/}
+    <IndukProps/>
+    
+  </React.StrictMode>,
+)
